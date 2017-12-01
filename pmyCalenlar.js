e60ccee316bfc8e7817b3250d89dce7481827147
string <766585557@qq.com>
2017-12-01 16:46:48 +0800

    init

--- pmyCalenlar.js
+++ pmyCalenlar.js
@@ -1,1 +1,16 @@
-;()();
+;(function () {
+    
+})();
+
+if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    
+    // AMD. Register as an anonymous module.
+    define(function() {
+        return FastClick;
+    });
+} else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = FastClick.attach;
+    module.exports.FastClick = FastClick;
+} else {
+    window.FastClick = FastClick;
+}
